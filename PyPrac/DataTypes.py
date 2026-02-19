8abd3ee8b3776c2614cb9ba5db4844857abcb987
BrahimJandri <bzaza066@gmail.com>
2026-02-19 12:09:00 +0000

    practice with datatypes

---
+++ PyPrac/DataTypes.py
@@ -0,0 +1,17 @@
+Age = 24
+height = 170
+name = "brahim"
+Student = True
+Married = None
+
+print(Age)
+print(height)
+print(name)
+print(Student)
+print(Married)
+print(type(Age))
+print(type(height))
+print(type(name))
+print(type(Student))
+print(type(Married))
+print(len(name))
